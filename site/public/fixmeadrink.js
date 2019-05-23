bd6bea8984dc96547d9ad75f7777fff2733866a5
Mark <mn16660@my.bristol.ac.uk>
2019-05-23 15:18:32 +0100

    Animated page completed

--- site/public/fixmeadrink.js
+++ site/public/fixmeadrink.js
@@ -1,5 +1,14 @@
 "use strict";
 var loaded = 0;
+var loaded2 = 0;
+var loaded3 = 0;
+var anim1;
+var anim2;
+var anim3;
+
+var base;
+var flavourChoice;
+
 function tilt(item) {
     var obj = document.getElementById(item);
     obj.style.transform = "rotate(20deg)";
@@ -13,19 +22,94 @@ function untilt(item) {
 function playanim() {
     if (loaded === 0) {
         loaded = 1;
-        spirit.loadAnimation({ autoPlay: true, loop: true, yoyo: true, container: document.getElementById("1"), path:       './Images/smoke.json' })
+        spirit.loadAnimation({ autoPlay: false, loop: true, yoyo: true, container: document.getElementById("1"), path:       './Images/smoke.json' })
         .then(timeline => {
-            window.onmousemove = ({ clientX }) => {
-            timeline.progress(clientX / window.innerWidth)
-            }
+            anim1 = timeline;
         })
+    }
+    if (loaded === 1) {
+        anim1.play(0);
+    }
+}
+
+function playanim2() {
+    if (loaded2 === 0) {
+        loaded2 = 1;
+        spirit.loadAnimation({ autoPlay: false, loop: true, yoyo: true, path: './Images/strong.json' })
+        .then(timeline => {
+            anim2 = timeline;
+        })
+    }
+    if (loaded2 === 1) {
+        anim2.play(0);
+    }
+}
+
+function playanim3() {
+    if (loaded3 === 0) {
+        loaded3 = 1;
+        spirit.loadAnimation({ autoPlay: false, loop: true, yoyo: true, path: './Images/sweet.json' })
+        .then(timeline => {
+            anim3 = timeline;
+        })
+    }
+    if (loaded3 === 1) {
+        anim3.play(0);
+    }
+}
+
+function stopanim(val) {
+    if (val === 1) {
+        anim1.pause(0);
+    }
+    if (val === 2) {
+        anim2.pause(0);
+    }
+    if (val === 3) {
+        anim3.pause(0);
     }
 }
 
 function selectbase(item) {
-    console.log(item);
+    base = item;
+    console.log(base);
     document.getElementById("choosebase").style.visibility = "hidden";
     document.getElementById("choosetaste").style.visibility = "visible";
     document.getElementById("name").innerHTML = "What flavour are you going for?";
+}
+
+function selectflavour(flavour) {
+    flavourChoice = flavour;
+    console.log(flavourChoice);
+    document.getElementById("choosetaste").style.visibility = "hidden";
+    document.getElementById("result").style.visibility = "visible";
+    if (base === "vodka") {
+        if (flavourChoice === "strong") {
+            document.getElementById("resulttitle").innerHTML = "Vodka Martini";
+            document.getElementById("resultwords").innerHTML = "Perfect for the midweek blues. Vodka and dry vermouth.";
+        }
+        else if (flavourChoice === "sweet") {
+            document.getElementById("resulttitle").innerHTML = "French Martini";
+            document.getElementById("resultwords").innerHTML = "For the sweet tooth cravings. Vodka, raspberry liqueur and a dash of pineapple juice.";
+        }
+        else if (flavourChoice === "smokey") {
+            document.getElementById("resulttitle").innerHTML = "Smokey Maple";
+            document.getElementById("resultwords").innerHTML = "A hint of sweetness to a strong oak flavour. Smoked vodka and a tablespoon of maple syrup.";
+        }
+    }
+    else if (base === "whisky") {
+        if (flavourChoice === "strong") {
+            document.getElementById("resulttitle").innerHTML = "Manhattan";
+            document.getElementById("resultwords").innerHTML = "A true classic. Bourbon or rye whisky, sweet vermouth, Angostura bitters.";
+        }
+        else if (flavourChoice === "sweet") {
+            document.getElementById("resulttitle").innerHTML = "Old Fashioned";
+            document.getElementById("resultwords").innerHTML = "Does this count as dessert? Bourbon, bitters, fresh orange, a cherry, a dash of syrup.";
+        }
+        else if (flavourChoice === "smokey") {
+            document.getElementById("resulttitle").innerHTML = "Smoked Boulevardier";
+            document.getElementById("resultwords").innerHTML = "Delightful on a cold night. Smoky whisky, sweet vermouth, campari. Add a lemon peel for garnish.";
+        }
+    }
 }
 
